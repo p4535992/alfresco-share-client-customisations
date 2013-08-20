3a927f0c134cc92413c78de1d19d690c162d65db
Simon White <simon.white@gmail.com>
2013-08-20 14:24:51 +0100

    Correct license headers

--- source/web/modules/simple-editor.js
+++ source/web/modules/simple-editor.js
@@ -1,113 +1,24 @@
-/* Gutter Plugin */
-Alfresco.gutter = function(myEditor)
-{
-   var Dom = YAHOO.util.Dom,
-      Anim = YAHOO.util.Anim;
-   
-   return (
-   {
-      status: false,
-      gutter: null,
-         
-      createGutter: function()
-      {
-         this.gutter = new YAHOO.widget.Overlay('gutter1',
-         {
-            height: '384px',
-            width: '300px',
-            context: [Dom.get(myEditor.getContainer()), 'tl', 'tr'],
-            position: 'absolute',
-            visible: false
-         });
-         
-         this.gutter.hideEvent.subscribe(function()
-         {
-            myEditor.deactivateButton('alfresco-imagelibrary');
-            Dom.setStyle("image_results", "overflow", "hidden");
-            Dom.setStyle("gutter1", "visibility", "visible");
-            var anim = new Anim('gutter1',
-            {
-               width:
-               {
-                  from: 300,
-                  to: 0
-               },
-               opacity:
-               {
-                  from: 1,
-                  to: 0
-               }
-            }, 1);
-            anim.onComplete.subscribe(function()
-            {
-               Dom.setStyle("gutter1", "visibility", "hidden");
-            });
-            anim.animate();
-         }, this, true);
-
-         this.gutter.showEvent.subscribe(function()
-         {
-            myEditor.activateButton('alfresco-imagelibrary');
-            this.gutter.cfg.setProperty('context', [Dom.get(myEditor.getContainer()), 'tl', 'tr']);
-            Dom.setStyle("gutter1", "visibility", "visible");
-            var anim = new Anim('gutter1',
-            {
-               width:
-               {
-                  from: 0,
-                  to: 300
-               },
-               opacity:
-               {
-                  from: 0,
-                  to: 1
-               }
-            }, 1);
-            anim.onComplete.subscribe(function()
-            {
-               Dom.setStyle("image_results", "overflow", "auto");
-            });
-            anim.animate();
-         }, this, true);
-         
-         var libraryTitle = Alfresco.util.message("imagelib.title");
-         this.gutter.setBody('<div class="yui-toolbar-container"><div class="yui-toolbar-titlebar"><h2>' + libraryTitle + '</h2></div></div><div id="image_results"></div>');
-         this.gutter.render(document.body);
-         Dom.setStyle(this.gutter.element, 'width', '0px');
-      },
-         
-      open: function()
-      {
-         this.gutter.show();
-         this.status = true;
-      },
-         
-      close: function()
-      {
-         this.gutter.hide();
-         this.status = false;
-      },
-         
-      toggle: function()
-      {
-         if (this.status)
-         {
-            this.close();
-         }
-         else
-         {
-            this.open();
-         }
-      }
-   });
-};
-
-/**
- * Alfresco top-level util namespace.
+/*
+ * Copyright (C) 2008-2010 Surevine Limited.
+ *   
+ * Although intended for deployment and use alongside Alfresco this module should
+ * be considered 'Not a Contribution' as defined in Alfresco'sstandard contribution agreement, see
+ * http://www.alfresco.org/resource/AlfrescoContributionAgreementv2.pdf
  * 
- * @namespace Alfresco
- * @class Alfresco.util
- */
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 Alfresco.util = Alfresco.util || {};
 
 Alfresco.util.createImageEditor = function(id, options)
